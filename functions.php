a9ee8fa0b4c238ad8f4b0236f1d7698ee038ee08
Waqas Tariq <waqastariq0987@gmail.com>
2021-04-07 13:08:20 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,4 @@
 <?php
-/*This file is part of ChildThemeSalient, salient child theme.
-
-All functions of this file will be loaded before of parent theme functions.
-Learn more at https://codex.wordpress.org/Child_Themes.
-
-Note: this function loads the parent stylesheet before, then child theme stylesheet
-(leave it in place unless you know what you are doing.)
-*/
 
 if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
 	function ChildThemeSalient_enqueue_child_styles() {
